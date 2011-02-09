679a36d869b3e2b1743a434e37b306398bc6ee01
Johannes Lerch <lerch@st.informatik.tu-darmstadt.de>
2011-02-09 17:02:01 +0100

    Refactoring of ModelStores for call and overrides completion. Extraction of IO-Parts.

--- org.eclipse.recommenders.rcp.codecompletion.calls/src/org/eclipse/recommenders/internal/rcp/codecompletion/calls/CallsCompletionModule.java
+++ org.eclipse.recommenders.rcp.codecompletion.calls/src/org/eclipse/recommenders/internal/rcp/codecompletion/calls/CallsCompletionModule.java
@@ -10,12 +10,19 @@
  */
 package org.eclipse.recommenders.internal.rcp.codecompletion.calls;
 
+import java.io.IOException;
+import java.net.URL;
+
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.recommenders.internal.rcp.views.recommendations.IRecommendationsViewContentProvider;
 import org.eclipse.recommenders.rcp.codecompletion.IIntelligentCompletionEngine;
+import org.osgi.framework.FrameworkUtil;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Scopes;
 import com.google.inject.multibindings.Multibinder;
+import com.google.inject.name.Names;
 
 public class CallsCompletionModule extends AbstractModule {
     @Override
@@ -27,7 +34,8 @@ public class CallsCompletionModule extends AbstractModule {
 
     private void configureModelStore() {
         bind(CallsModelStore.class).in(Scopes.SINGLETON);
-
+        bind(ICallsModelLoader.class).to(CallsModelLoader.class).in(Scopes.SINGLETON);
+        bind(URL.class).annotatedWith(Names.named("calls.model.fileUrl")).toInstance(getCallsModelFileUrl());
     }
 
     private void configureCompletionEngine() {
@@ -39,5 +47,14 @@ public class CallsCompletionModule extends AbstractModule {
     private void configureRecommendationsViewPublisher() {
         Multibinder.newSetBinder(binder(), IRecommendationsViewContentProvider.class).addBinding()
                 .to(RecommendationsViewPublisherForCalls.class);
+    }
+
+    private URL getCallsModelFileUrl() {
+        final Path basedir = new Path("/data/models.zip");
+        try {
+            return FileLocator.resolve(FileLocator.find(FrameworkUtil.getBundle(getClass()), basedir, null));
+        } catch (final IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
--- org.eclipse.recommenders.rcp.codecompletion.overrides/src/org/eclipse/recommenders/internal/rcp/codecompletion/overrides/OverridesModelStore.java
+++ org.eclipse.recommenders.rcp.codecompletion.overrides/src/org/eclipse/recommenders/internal/rcp/codecompletion/overrides/OverridesModelStore.java
@@ -14,40 +14,29 @@ import static org.eclipse.recommenders.commons.utils.Checks.ensureIsTrue;
 import static org.eclipse.recommenders.commons.utils.Throws.throwUnhandledException;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.lang.reflect.Type;
-import java.net.URL;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.jar.JarFile;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipFile;
 
 import javax.annotation.Nullable;
 
-import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.time.StopWatch;
-import org.eclipse.core.runtime.FileLocator;
-import org.eclipse.core.runtime.Path;
 import org.eclipse.recommenders.commons.utils.FixedSizeLinkedHashMap;
 import org.eclipse.recommenders.commons.utils.annotations.Clumsy;
-import org.eclipse.recommenders.commons.utils.gson.GsonUtil;
 import org.eclipse.recommenders.commons.utils.names.ITypeName;
-import org.eclipse.recommenders.commons.utils.names.VmTypeName;
 import org.eclipse.recommenders.internal.rcp.codecompletion.overrides.net.ClassOverridesNetwork;
 import org.eclipse.recommenders.internal.rcp.codecompletion.overrides.net.ClassOverridesNetworkBuilder;
 import org.eclipse.recommenders.internal.rcp.codecompletion.overrides.net.ClassOverridesObservation;
-import org.osgi.framework.Bundle;
-import org.osgi.framework.FrameworkUtil;
 
-import com.google.common.collect.Sets;
 import com.google.gson.reflect.TypeToken;
+import com.google.inject.Inject;
 
 @Clumsy
 public class OverridesModelStore {
-    private static final String FILENAME_PREFIX = "class-overrides-";
+
+    @Inject
+    private IOverridesModelLoader loader;
 
     private Set<ITypeName> supportedTypes;
 
@@ -55,25 +44,8 @@ public class OverridesModelStore {
 
     private void init() {
         if (supportedTypes == null) {
-            supportedTypes = computeSupportedTypes();
+            supportedTypes = loader.readAvailableTypes();
         }
-    }
-
-    protected Set<ITypeName> computeSupportedTypes() {
-        final Set<ITypeName> types = Sets.newTreeSet();
-        final ZipFile modelsZip = getModelsFile();
-        final List<? extends ZipEntry> entries = Collections.list(modelsZip.entries());
-        for (final ZipEntry entry : entries) {
-            final String fileName = entry.getName();
-            if (!fileName.endsWith("json")) {
-                continue;
-            }
-            final String typeNameAsString = StringUtils.substringBeforeLast(fileName, ".").replace('.', '/')
-                    .substring(FILENAME_PREFIX.length());
-            final VmTypeName typeName = VmTypeName.get(typeNameAsString);
-            types.add(typeName);
-        }
-        return types;
     }
 
     public boolean hasModel(@Nullable final ITypeName name) {
@@ -86,44 +58,22 @@ public class OverridesModelStore {
         //
         ClassOverridesNetwork network = loadedNetworks.get(name);
         if (network == null) {
-            network = loadNetworkFromDisk(name);
+            network = loadNetwork(name);
             loadedNetworks.put(name, network);
         }
         return network;
     }
 
-    private JarFile getModelsFile() {
-        try {
-            final Bundle bundle = FrameworkUtil.getBundle(this.getClass());
-            final Path basedir = new Path("/data/models.zip");
-            final URL path = FileLocator.resolve(FileLocator.find(bundle, basedir, null));
-            final String file = path.getFile();
-            return new JarFile(file);
-        } catch (final IOException e) {
-            throw throwUnhandledException(e);
-        }
-    }
-
-    private ClassOverridesNetwork loadNetworkFromDisk(final ITypeName name) {
+    private ClassOverridesNetwork loadNetwork(final ITypeName name) {
         final StopWatch stopwatch = new StopWatch();
         stopwatch.start();
-        final Type listType = new TypeToken<List<ClassOverridesObservation>>() {
-        }.getType();
+
         try {
             stopwatch.split();
-            final InputStream is = getModelInputStream(name);
-            final List<ClassOverridesObservation> observations = GsonUtil.deserialize(is, listType);
+            final List<ClassOverridesObservation> observations = loadObservations(name);
             System.out.printf("deserialization of '%s' took %s\n", name, stopwatch);
-            if (observations.size() == 0) {
-                // XXX sanitize bad models! need to ensure minimum quality for models.
-                observations.add(new ClassOverridesObservation());
-            }
             stopwatch.unsplit();
-            final ClassOverridesNetworkBuilder b = new ClassOverridesNetworkBuilder(name, observations);
-            b.createPatternsNode();
-            b.createMethodNodes();
-            final ClassOverridesNetwork network = b.build();
-            return network;
+            return createNetwork(name, observations);
         } catch (final IOException x) {
             throw throwUnhandledException(x);
         } finally {
@@ -132,11 +82,24 @@ public class OverridesModelStore {
         }
     }
 
-    protected InputStream getModelInputStream(final ITypeName name) throws IOException {
-        final JarFile modelsFile = getModelsFile();
-        final String entryName = FILENAME_PREFIX + name.getIdentifier().replace('/', '.').concat(".json");
-        final ZipEntry entry = modelsFile.getEntry(entryName);
-        final InputStream is = modelsFile.getInputStream(entry);
-        return is;
+    private List<ClassOverridesObservation> loadObservations(final ITypeName name) throws IOException {
+        final Type listType = new TypeToken<List<ClassOverridesObservation>>() {
+        }.getType();
+        final List<ClassOverridesObservation> observations = loader.loadObjectForTypeName(name, listType);
+        if (observations.size() == 0) {
+            // XXX sanitize bad models! need to ensure minimum quality for
+            // models.
+            observations.add(new ClassOverridesObservation());
+        }
+        return observations;
     }
+
+    private ClassOverridesNetwork createNetwork(final ITypeName name, final List<ClassOverridesObservation> observations) {
+        final ClassOverridesNetworkBuilder b = new ClassOverridesNetworkBuilder(name, observations);
+        b.createPatternsNode();
+        b.createMethodNodes();
+        final ClassOverridesNetwork network = b.build();
+        return network;
+    }
+
 }
