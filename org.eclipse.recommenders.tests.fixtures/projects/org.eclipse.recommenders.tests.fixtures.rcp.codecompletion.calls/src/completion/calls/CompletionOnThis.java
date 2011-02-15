be74bf664f49831eec7d70803009bafdbee39caa
Marcel Bruch <bruch@st.informatik.tu-darmstadt.de>
2011-02-15 17:23:18 +0100

    nuked missing license header .. again...

--- org.eclipse.recommenders.tests.fixtures/projects/org.eclipse.recommenders.tests.fixtures.rcp.codecompletion.calls/src/completion/calls/CompletionOnThis.java
+++ org.eclipse.recommenders.tests.fixtures/projects/org.eclipse.recommenders.tests.fixtures.rcp.codecompletion.calls/src/completion/calls/CompletionOnThis.java
@@ -1,18 +1,28 @@
+/**
+ * Copyright (c) 2010 Darmstadt University of Technology.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Marcel Bruch - initial API and implementation.
+ */
 package completion.calls;
 
 import org.eclipse.jface.wizard.Wizard;
 
 public class CompletionOnThis extends Wizard {
 
-	@Override
+    @Override
     public void addPages() {
-         <^Space|addPage.*%>
+         Space|addPage.*%>
     }
 
-	@Override
-	public boolean performFinish() {
+    @Override
+    public boolean performFinish() {
 
-		return false;
-	}
+        return false;
+    }
 
 }
