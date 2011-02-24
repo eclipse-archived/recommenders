755d6673a68c4a4ce7e834016c79775e3a96d44b
Marcel Bruch <bruch@st.informatik.tu-darmstadt.de>
2011-02-24 19:15:27 +0100

    fixed chain completion fail on field completions. filtering declared fields and locals from list of proposals.

--- org.eclipse.recommenders.rcp.codecompletion.chain/src/org/eclipse/recommenders/internal/rcp/codecompletion/chain/algorithm/ChainCompletionContext.java
+++ org.eclipse.recommenders.rcp.codecompletion.chain/src/org/eclipse/recommenders/internal/rcp/codecompletion/chain/algorithm/ChainCompletionContext.java
@@ -10,10 +10,13 @@
  */
 package org.eclipse.recommenders.internal.rcp.codecompletion.chain.algorithm;
 
+import static org.eclipse.recommenders.commons.utils.Checks.cast;
+
 import java.util.List;
 
 import org.eclipse.jdt.core.IType;
 import org.eclipse.jdt.internal.codeassist.complete.CompletionOnSingleNameReference;
+import org.eclipse.jdt.internal.compiler.ast.FieldDeclaration;
 import org.eclipse.jdt.internal.compiler.ast.LocalDeclaration;
 import org.eclipse.recommenders.commons.utils.names.IMethodName;
 import org.eclipse.recommenders.commons.utils.names.ITypeName;
@@ -55,7 +58,7 @@ public class ChainCompletionContext {
       return;
     }
     if (!findEnclosingMethod()) {
-      return;
+      // return;
     }
     if (!findReceiverClass()) {
       return;
@@ -130,6 +133,14 @@ public class ChainCompletionContext {
       if (!matchesPrefix(field)) {
         continue;
       }
+      if (ctx.getCompletionNodeParent() instanceof FieldDeclaration) {
+        final FieldDeclaration node = cast(ctx.getCompletionNodeParent());
+        final String name = String.valueOf(node.name);
+        if (name.equals(field.getName().toString())) {
+          continue;
+        }
+      }
+
       final FieldChainElement chainElement = new FieldChainElement(field);
       accessibleFields.add(chainElement);
     }
@@ -191,6 +202,15 @@ public class ChainCompletionContext {
       if (!localName.startsWith(ctx.getPrefixToken())) {
         continue;
       }
+
+      if (ctx.getCompletionNodeParent() instanceof LocalDeclaration) {
+        final LocalDeclaration node = cast(ctx.getCompletionNodeParent());
+        final String name = String.valueOf(node.name);
+        if (name.equals(localName)) {
+          continue;
+        }
+      }
+
       final IChainElement element = new LocalChainElement(localName, localType);
       accessibleLocals.add(element);
     }
