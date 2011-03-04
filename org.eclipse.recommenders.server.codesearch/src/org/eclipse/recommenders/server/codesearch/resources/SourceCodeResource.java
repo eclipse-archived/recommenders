81791472f0cf326fa21a476eca2792efcafe865d
Johannes Lerch <lerch@st.informatik.tu-darmstadt.de>
2011-03-04 18:07:39 +0100

    Added interface to retrieve source files.

--- org.eclipse.recommenders.server.codesearch/src/org/eclipse/recommenders/server/codesearch/resources/SourceCodeResource.java
+++ org.eclipse.recommenders.server.codesearch/src/org/eclipse/recommenders/server/codesearch/resources/SourceCodeResource.java
@@ -10,49 +10,32 @@
  */
 package org.eclipse.recommenders.server.codesearch.resources;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Response;
+
+import org.eclipse.recommenders.server.codesearch.LocalFileService;
 
 import com.google.inject.Inject;
-import com.google.inject.Injector;
-import com.google.inject.name.Named;
 
 @Path("source")
 public class SourceCodeResource {
 
     @Inject
-    @Named("codesearch.index")
-    private File indexFolder;
-
-    @Inject
-    private Injector injector;
+    private LocalFileService localFileService;
 
     @GET
     @Path("{id}")
     public String getSource(@PathParam("id") final String id) {
         final String filename = id.replaceAll("/", ".") + ".java";
-        return readFile(filename);
-    }
-
-    private String readFile(final String id) {
-        final File basedir = new File(indexFolder, "sources");
-        try {
-            final FileReader reader = new FileReader(new File(basedir, id));
-            final BufferedReader bufferedReader = new BufferedReader(reader);
-            final StringBuilder builder = new StringBuilder();
-            String line = null;
-            while ((line = bufferedReader.readLine()) != null) {
-                builder.append(line);
-            }
-            return builder.toString();
-        } catch (final IOException e) {
-            throw new RuntimeException(e);
+        final String fileContent = localFileService.readFile(filename);
+        if (fileContent == null) {
+            throw new WebApplicationException(Response.Status.NOT_FOUND);
+        } else {
+            return fileContent;
         }
     }
+
 }
