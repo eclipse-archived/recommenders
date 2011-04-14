e95b347f52e996f04708bfcf68ccc5a7488be8b2
Marcel Bruch <bruch@st.informatik.tu-darmstadt.de>
2011-04-14 12:33:23 +0200

    added newTuple convenience method - support better usage of static imports

--- org.eclipse.recommenders.commons.utils/src/org/eclipse/recommenders/commons/utils/Tuple.java
+++ org.eclipse.recommenders.commons.utils/src/org/eclipse/recommenders/commons/utils/Tuple.java
@@ -18,8 +18,12 @@ public class Tuple<T0, T1> {
 
     private T1 t1;
 
-    public static <T0, S0 extends T0, T1, S1 extends T1> Tuple<T0, T1> create(final S0 t0, final S1 t1) {
+    public static <T0, S0 extends T0, T1, S1 extends T1> Tuple<T0, T1> newTuple(final S0 t0, final S1 t1) {
         return new Tuple<T0, T1>(t0, t1);
+    }
+
+    public static <T0, S0 extends T0, T1, S1 extends T1> Tuple<T0, T1> create(final S0 t0, final S1 t1) {
+        return newTuple(t0, t1);
     }
 
     protected Tuple() {
