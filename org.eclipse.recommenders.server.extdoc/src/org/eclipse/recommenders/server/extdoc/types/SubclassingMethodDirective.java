bee78eb042a5d4f7e5df7f142af6400a56139020
Stefan Henß <stefan.henss@gmail.com>
2011-06-12 15:27:40 +0200

    Types for server documents.
    Change-Id: I182e4b274a52177f5babf4232ac814633720e772

---
+++ org.eclipse.recommenders.server.extdoc/src/org/eclipse/recommenders/server/extdoc/types/SubclassingMethodDirective.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2011 Stefan Henss.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Stefan Henss - initial API and implementation.
+ */
+package org.eclipse.recommenders.server.extdoc.types;
+
+import java.util.Map;
+
+import com.google.gson.annotations.SerializedName;
+
+import org.eclipse.jdt.core.IMethod;
+import org.eclipse.recommenders.rcp.extdoc.IProvider;
+
+public class SubclassingMethodDirective {
+
+    @SerializedName("_id")
+    public String id;
+    @SerializedName("_rev")
+    public String rev;
+
+    public IProvider provider;
+    public IMethod method;
+
+    public int subclasses;
+    public int overrides;
+    public int superCalls;
+    public Map<IMethod, Integer> calls;
+
+}
