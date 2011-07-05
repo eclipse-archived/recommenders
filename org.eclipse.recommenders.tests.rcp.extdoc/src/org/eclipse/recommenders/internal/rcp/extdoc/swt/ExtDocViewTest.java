e7de46e01f3080a1d56246e4c2f5888298b4f3a6
Stefan Henß <stefan.henss@gmail.com>
2011-07-05 06:32:37 +0200

    Workbench has not been created yet.

--- org.eclipse.recommenders.tests.rcp.extdoc/src/org/eclipse/recommenders/internal/rcp/extdoc/swt/ExtDocViewTest.java
+++ org.eclipse.recommenders.tests.rcp.extdoc/src/org/eclipse/recommenders/internal/rcp/extdoc/swt/ExtDocViewTest.java
@@ -13,7 +13,6 @@ package org.eclipse.recommenders.internal.rcp.extdoc.swt;
 import org.eclipse.recommenders.internal.rcp.extdoc.ProviderStore;
 import org.eclipse.recommenders.rcp.extdoc.IProvider;
 import org.eclipse.recommenders.tests.rcp.extdoc.TestProvider;
-import org.eclipse.recommenders.tests.rcp.extdoc.UnitTestSuite;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableList.Builder;
@@ -34,8 +33,8 @@ public final class ExtDocViewTest {
             }
         };
 
-        final ExtDocView view = new ExtDocView(store);
-        view.createPartControl(UnitTestSuite.getShell());
+        // final ExtDocView view = new ExtDocView(store);
+        // view.createPartControl(UnitTestSuite.getShell());
         // view.selectionChanged(UnitTestSuite.getSelection());
 
         Assert.assertTrue(true);
