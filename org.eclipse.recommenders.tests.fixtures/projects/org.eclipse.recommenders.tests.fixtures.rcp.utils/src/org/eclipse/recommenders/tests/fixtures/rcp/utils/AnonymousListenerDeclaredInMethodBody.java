be4a4f97dcbe21d1b9b2be12a1806067928a548d
Marcel Bruch <bruch@st.informatik.tu-darmstadt.de>
2011-07-08 10:28:01 +0200

    added fixture project for rcp utils

---
+++ org.eclipse.recommenders.tests.fixtures/projects/org.eclipse.recommenders.tests.fixtures.rcp.utils/src/org/eclipse/recommenders/tests/fixtures/rcp/utils/AnonymousListenerDeclaredInMethodBody.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2010, 2011 Darmstadt University of Technology.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Marcel Bruch - Initial API and implementation
+ */
+package org.eclipse.recommenders.tests.fixtures.rcp.utils;
+
+import org.eclipse.swt.events.ControlEvent;
+import org.eclipse.swt.events.ControlListener;
+
+public class AnonymousListenerDeclaredInMethodBody {
+
+    public void __test() {
+
+        final ControlListener l = new ControlListener() {
+
+            @Override
+            public void controlMoved(final ControlEvent e) {
+                // TODO Auto-generated method stub
+
+            }
+
+            @Override
+            public void controlResized(final ControlEvent e) {
+                // TODO Auto-generated method stub
+
+            }
+        };
+
+    }
+}
---
+++ org.eclipse.recommenders.tests.fixtures/projects/org.eclipse.recommenders.tests.fixtures.rcp.utils/src/org/eclipse/recommenders/tests/fixtures/rcp/utils/ComparatorWithGenerics.java
@@ -0,0 +1,20 @@
+/**
+ * Copyright (c) 2010, 2011 Darmstadt University of Technology.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *     Marcel Bruch - Initial API and implementation
+ */
+package org.eclipse.recommenders.tests.fixtures.rcp.utils;
+
+public class ComparatorWithGenerics implements java.util.Comparator<String> {
+
+    @Override
+    public int compare(String arg0, String arg1) {
+        return 0;
+    }
+
+}
