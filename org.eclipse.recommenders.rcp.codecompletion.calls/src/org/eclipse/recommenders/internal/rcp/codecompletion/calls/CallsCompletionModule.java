1ebd05996fc08faaa4851eff3b967fc1e6511e26
Sebastian Proksch <sebastian@proks.ch>
2011-11-07 10:19:49 +0100

    * added version to pathes on client side for models and index * calls completion: added manual tests for query generation

--- org.eclipse.recommenders.rcp.codecompletion.calls/src/org/eclipse/recommenders/internal/rcp/codecompletion/calls/CallsCompletionModule.java
+++ org.eclipse.recommenders.rcp.codecompletion.calls/src/org/eclipse/recommenders/internal/rcp/codecompletion/calls/CallsCompletionModule.java
@@ -41,64 +41,65 @@ import com.google.inject.name.Names;
 
 public class CallsCompletionModule extends AbstractModule {
 
-    public static final String CALLS_STORE_LOCATION = "calls.store.location";
+	public static final String MODEL_VERSION = "0.4";
+	public static final String CALLS_STORE_LOCATION = "calls.store.location";
 
-    @Override
-    protected void configure() {
-        configurePreferences();
-        configureProjectServices();
-        configureArchiveModelStore();
-        configureRecommendationsViewPublisher();
-    }
+	@Override
+	protected void configure() {
+		configurePreferences();
+		configureProjectServices();
+		configureArchiveModelStore();
+		configureRecommendationsViewPublisher();
+	}
 
-    private void configurePreferences() {
-        final ClientConfiguration config = new ClientConfiguration();
-        final IPreferenceStore store = CallsCompletionPlugin.getDefault().getPreferenceStore();
-        store.addPropertyChangeListener(new ClientConfigurationPreferenceListener(config, store));
-        bind(ClientConfiguration.class).annotatedWith(UdcServer.class).toInstance(config);
-    }
+	private void configurePreferences() {
+		final ClientConfiguration config = new ClientConfiguration();
+		final IPreferenceStore store = CallsCompletionPlugin.getDefault().getPreferenceStore();
+		store.addPropertyChangeListener(new ClientConfigurationPreferenceListener(config, store));
+		bind(ClientConfiguration.class).annotatedWith(UdcServer.class).toInstance(config);
+	}
 
-    private void configureProjectServices() {
-        bind(ProjectServices.class).in(Scopes.SINGLETON);
-        final Multibinder<IRecommendersProjectLifeCycleListener> multibinder = Multibinder.newSetBinder(binder(),
-                IRecommendersProjectLifeCycleListener.class);
-        multibinder.addBinding().to(ProjectServices.class);
-    }
+	private void configureProjectServices() {
+		bind(ProjectServices.class).in(Scopes.SINGLETON);
+		final Multibinder<IRecommendersProjectLifeCycleListener> multibinder = Multibinder.newSetBinder(binder(),
+				IRecommendersProjectLifeCycleListener.class);
+		multibinder.addBinding().to(ProjectServices.class);
+	}
 
-    private void configureArchiveModelStore() {
-        bind(IModelArchiveStore.class).to(ModelArchiveStore.class).in(Scopes.SINGLETON);
+	private void configureArchiveModelStore() {
+		bind(IModelArchiveStore.class).to(ModelArchiveStore.class).in(Scopes.SINGLETON);
 
-        final IPath stateLocation = Platform.getStateLocation(FrameworkUtil.getBundle(getClass()));
-        bind(File.class).annotatedWith(Names.named(CALLS_STORE_LOCATION)).toInstance(
-                new File(stateLocation.toFile(), "models"));
+		final IPath stateLocation = Platform.getStateLocation(FrameworkUtil.getBundle(getClass()));
+		bind(File.class).annotatedWith(Names.named(CALLS_STORE_LOCATION)).toInstance(
+				new File(stateLocation.toFile(), MODEL_VERSION + "/models/"));
 
-        bind(File.class).annotatedWith(ClasspathDependencyStoreLocation.class).toInstance(
-                new File(stateLocation.toFile(), "dependencyIndex"));
-        bind(ClasspathDependencyStore.class).in(Scopes.SINGLETON);
-        install(new FactoryModuleBuilder().build(ProjectModelFacadeFactory.class));
-        install(new FactoryModuleBuilder().build(RemoteResolverJobFactory.class));
-    }
+		bind(File.class).annotatedWith(ClasspathDependencyStoreLocation.class).toInstance(
+				new File(stateLocation.toFile(), MODEL_VERSION + "/dependencyIndex/"));
+		bind(ClasspathDependencyStore.class).in(Scopes.SINGLETON);
+		install(new FactoryModuleBuilder().build(ProjectModelFacadeFactory.class));
+		install(new FactoryModuleBuilder().build(RemoteResolverJobFactory.class));
+	}
 
-    private void configureRecommendationsViewPublisher() {
-        Multibinder.newSetBinder(binder(), IRecommendationsViewContentProvider.class).addBinding()
-                .to(RecommendationsViewPublisherForCalls.class);
-    }
+	private void configureRecommendationsViewPublisher() {
+		Multibinder.newSetBinder(binder(), IRecommendationsViewContentProvider.class).addBinding()
+				.to(RecommendationsViewPublisherForCalls.class);
+	}
 
-    @BindingAnnotation
-    @Target(PARAMETER)
-    @Retention(RUNTIME)
-    public static @interface ClasspathDependencyStoreLocation {
-    }
+	@BindingAnnotation
+	@Target(PARAMETER)
+	@Retention(RUNTIME)
+	public static @interface ClasspathDependencyStoreLocation {
+	}
 
-    @BindingAnnotation
-    @Target(PARAMETER)
-    @Retention(RUNTIME)
-    public static @interface UdcServer {
-    }
+	@BindingAnnotation
+	@Target(PARAMETER)
+	@Retention(RUNTIME)
+	public static @interface UdcServer {
+	}
 
-    @BindingAnnotation
-    @Target(PARAMETER)
-    @Retention(RUNTIME)
-    public static @interface PreferenceStore {
-    }
+	@BindingAnnotation
+	@Target(PARAMETER)
+	@Retention(RUNTIME)
+	public static @interface PreferenceStore {
+	}
 }
---
+++ org.eclipse.recommenders.tests.fixtures/projects/org.eclipse.recommenders.tests.fixtures.rcp.codecompletion.calls/src/completion/calls/queries/helper/AbstractMouseListener.java
@@ -0,0 +1,19 @@
+package completion.calls.queries.helper;
+
+import org.eclipse.swt.events.MouseEvent;
+import org.eclipse.swt.events.MouseListener;
+
+public class AbstractMouseListener implements MouseListener {
+
+	@Override
+	public void mouseDoubleClick(MouseEvent e) {
+	}
+
+	@Override
+	public void mouseDown(MouseEvent e) {
+	}
+
+	@Override
+	public void mouseUp(MouseEvent e) {
+	}
+}
