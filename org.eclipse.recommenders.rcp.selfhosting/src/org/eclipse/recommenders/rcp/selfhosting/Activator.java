0124601a8def6a381bba49213f2baccd8f51fb8f
Marcel Bruch <bruch@st.informatik.tu-darmstadt.de>
2011-11-08 12:30:05 +0100

    removed osgi console commands from self-hosting into new bundle server.console

--- org.eclipse.recommenders.rcp.selfhosting/src/org/eclipse/recommenders/rcp/selfhosting/Activator.java
+++ org.eclipse.recommenders.rcp.selfhosting/src/org/eclipse/recommenders/rcp/selfhosting/Activator.java
@@ -10,10 +10,7 @@
  */
 package org.eclipse.recommenders.rcp.selfhosting;
 
-import java.util.Properties;
-
 import org.eclipse.equinox.http.jetty.JettyConfigurator;
-import org.eclipse.osgi.framework.console.CommandProvider;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
@@ -24,7 +21,7 @@ public class Activator implements BundleActivator {
 
     @Override
     public void start(final BundleContext context) throws Exception {
-        context.registerService(CommandProvider.class.getName(), new ConsoleCommands(), new Properties());
+        // context.registerService(CommandProvider.class.getName(), new ConsoleCommands(), new Properties());
     }
 
     @Override
