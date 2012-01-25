a92fce4be6f071ea2ce87532fb1050f092c894e8
Marcel Bruch <bruch@st.informatik.tu-darmstadt.de>
2012-01-26 00:45:26 +0100

    [releng] added index.php copy command to publish.sh

--- etc/scripts/publish.sh
+++ etc/scripts/publish.sh
@@ -1,3 +1,6 @@
 #!/bin/sh
-rm -rf /home/data/httpd/download.eclipse.org/recommenders/updates/$target/*
-cp -r $WORKSPACE/dist/$repo_name/target/repository/* /home/data/httpd/download.eclipse.org/recommenders/updates/$target/
+echo "value of channel: $channel"
+echo "value of version: $version"
+rm -rf /home/data/httpd/download.eclipse.org/recommenders/updates/$channel/$version/*
+cp -r  $WORKSPACE/dist/org.eclipse.recommenders.repository.$channel.$version/target/repository/* /home/data/httpd/download.eclipse.org/recommenders/updates/$channel/$version/
+cp     $WORKSPACE/etc/scripts/index.php /home/data/httpd/download.eclipse.org/recommenders/updates/$channel/$version/
