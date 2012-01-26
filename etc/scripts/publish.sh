a6eb02230817e2bae2ef4b81198f77db7fe9ccbc
Marcel Bruch <bruch@st.informatik.tu-darmstadt.de>
2012-01-26 01:29:03 +0100

    [releng] added missing '.rcp' to copy script

--- etc/scripts/publish.sh
+++ etc/scripts/publish.sh
@@ -2,5 +2,5 @@
 echo "value of channel: $channel"
 echo "value of version: $version"
 rm -rf /home/data/httpd/download.eclipse.org/recommenders/updates/$channel/$version/*
-cp -r  $WORKSPACE/dist/org.eclipse.recommenders.repository.$channel.$version/target/repository/* /home/data/httpd/download.eclipse.org/recommenders/updates/$channel/$version/
+cp -r  $WORKSPACE/dist/org.eclipse.recommenders.repository.rcp.$channel.$version/target/repository/* /home/data/httpd/download.eclipse.org/recommenders/updates/$channel/$version/
 cp     $WORKSPACE/etc/scripts/index.php /home/data/httpd/download.eclipse.org/recommenders/updates/$channel/$version/
