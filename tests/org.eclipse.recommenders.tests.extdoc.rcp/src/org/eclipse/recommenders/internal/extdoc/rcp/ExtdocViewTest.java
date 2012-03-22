f537e7a4ee4e8d2fcf5484105e78a1f3f52fc0a3
Marcel Bruch <bruch@st.informatik.tu-darmstadt.de>
2012-03-22 06:41:15 +0100

    [extdoc] smoke tests for ExtdocUtils

--- tests/org.eclipse.recommenders.tests.extdoc.rcp/src/org/eclipse/recommenders/internal/extdoc/rcp/ExtdocViewTest.java
+++ tests/org.eclipse.recommenders.tests.extdoc.rcp/src/org/eclipse/recommenders/internal/extdoc/rcp/ExtdocViewTest.java
@@ -2,9 +2,13 @@ package org.eclipse.recommenders.internal.extdoc.rcp;
 
 import static org.junit.Assert.assertNotNull;
 
+import org.eclipse.recommenders.internal.extdoc.rcp.ui.ExtdocUtils;
 import org.eclipse.recommenders.internal.extdoc.rcp.ui.ExtdocView;
 import org.eclipse.recommenders.utils.rcp.RCPUtils;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.IViewPart;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.PartInitException;
@@ -34,5 +38,25 @@ public class ExtdocViewTest {
                 }
             }
         });
+    }
+
+    @Test
+    public void testUIUtilsSmoketest() {
+        Display.getDefault().syncExec(new Runnable() {
+
+            @Override
+            public void run() {
+                Shell s = new Shell();
+                ExtdocUtils.createButton(s, "", new SelectionAdapter() {
+                });
+                ExtdocUtils.createCLabel(s, "", false, null);
+                ExtdocUtils.createColor(SWT.COLOR_BLUE);
+                ExtdocUtils.createComposite(s, 12);
+                ExtdocUtils.createGridComposite(s, 2, 1, 1, 1, 1);
+                ExtdocUtils.createLabel(s, "", true);
+                ExtdocUtils.createLabel(s, "", true, true, SWT.COLOR_DARK_CYAN, false);
+            }
+        });
+
     }
 }
