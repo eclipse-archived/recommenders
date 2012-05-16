2608bc5586b5963990ee050ac6aa3b56a6c2ce81
Cheng Chen <chengchendoc@gmail.com>
2012-05-16 09:48:04 +0800

    [snipmatch] add more test cases for Effect unit test class

--- tests/org.eclipse.recommenders.tests.snipmatch.rcp/src/org/eclipse/recommenders/snipmatch/core/EffectTest.java
+++ tests/org.eclipse.recommenders.tests.snipmatch.rcp/src/org/eclipse/recommenders/snipmatch/core/EffectTest.java
@@ -144,6 +144,216 @@ public class EffectTest {
     }
 
     /**
+     * Run the EffectParameter getParameter(String) method test.
+     * 
+     * @throws Exception
+     * 
+     */
+    @Test
+    public void testGetParameter_4() throws Exception {
+        Effect fixture = GsonUtil.deserialize(json, Effect.class);
+        EffectParameter param = new EffectParameter();
+        param.setName("param_name");
+        param.setMajorType("major");
+        param.setMinorType("minor");
+        fixture.addParameter(param);
+
+        EffectParameter result = fixture.getParameter("param_name");
+
+        assertNotNull(result);
+        assertEquals("minor", result.getMinorType());
+        assertEquals("major:minor", result.getFullType());
+        assertEquals("major", result.getMajorType());
+        assertEquals("param_name", result.getName());
+    }
+
+ /**
+     * Run the int getParameterIndex(String) method test.
+     * 
+     * @throws Exception
+     * 
+     */
+    @Test
+    public void testGetParameterIndex_1() throws Exception {
+        Effect fixture = GsonUtil.deserialize(json, Effect.class);
+        int result = fixture.getParameterIndex("param_name");
+        assertEquals(-1, result);
+
+        EffectParameter param = new EffectParameter();
+        param.setName("param_name");
+        param.setMajorType("major");
+        param.setMinorType("minor");
+        fixture.addParameter(param);
+
+        result = fixture.getParameterIndex("param_name");
+        assertEquals(1, result);
+    }
+
+    /**
+     * Run the EffectParameter[] getParameters() method test.
+     * 
+     * @throws Exception
+     * 
+     */
+    @Test
+    public void testGetParameters_1() throws Exception {
+        Effect fixture = GsonUtil.deserialize(json, Effect.class);
+        EffectParameter param = new EffectParameter();
+        param.setName("param_name");
+        param.setMajorType("major");
+        param.setMinorType("minor");
+        fixture.addParameter(param);
+
+        EffectParameter[] result = fixture.getParameters();
+
+        assertNotNull(result);
+        assertEquals(2, result.length);
+        assertNotNull(result[0]);
+        assertEquals("", result[0].getMinorType());
+        assertEquals("expr", result[0].getFullType());
+        assertEquals("expr", result[0].getMajorType());
+        assertEquals("obj", result[0].getName());
+    }
+
+    /**
+     * Run the String getPattern(int) method test.
+     * 
+     * @throws Exception
+     * 
+     */
+    @Test
+    public void testGetPattern_1() throws Exception {
+        Effect fixture = GsonUtil.deserialize(json, Effect.class);
+        int index = 0;
+        String result = fixture.getPattern(index);
+
+        assertNotNull(result);
+        assertEquals("print $obj", result);
+    }
+
+    /**
+     * Run the String[] getPatterns() method test.
+     * 
+     * @throws Exception
+     * 
+     */
+    @Test
+    public void testGetPatterns_1() throws Exception {
+        Effect fixture = GsonUtil.deserialize(json, Effect.class);
+
+        String[] result = fixture.getPatterns();
+
+        assertNotNull(result);
+        assertEquals(1, result.length);
+        assertEquals("print $obj", result[0]);
+    }
+
+    /**
+     * Run the int numParameters() method test.
+     * 
+     * @throws Exception
+     * 
+     */
+    @Test
+    public void testNumParameters_1() throws Exception {
+        Effect fixture = GsonUtil.deserialize(json, Effect.class);
+        int result = fixture.numParameters();
+        assertEquals(1, result);
+    }
+
+    /**
+     * Run the int numPatterns() method test.
+     * 
+     * @throws Exception
+     * 
+     */
+    @Test
+    public void testNumPatterns_1() throws Exception {
+        Effect fixture = GsonUtil.deserialize(json, Effect.class);
+
+        int result = fixture.numPatterns();
+
+        assertEquals(1, result);
+    }
+
+    /**
+     * Run the void removeParameter(int) method test.
+     * 
+     * @throws Exception
+     * 
+     */
+    @Test
+    public void testRemoveParameter_1() throws Exception {
+        Effect fixture = GsonUtil.deserialize(json, Effect.class);
+        int result = fixture.numParameters();
+        assertEquals(1, result);
+        fixture.removeParameter(0);
+        result = fixture.numParameters();
+        assertEquals(0, result);
+    }
+
+    /**
+     * Run the void removeParameter(String) method test.
+     * 
+     * @throws Exception
+     * 
+     */
+    @Test
+    public void testRemoveParameter_2() throws Exception {
+        Effect fixture = GsonUtil.deserialize(json, Effect.class);
+        int result = fixture.numParameters();
+        assertEquals(1, result);
+        fixture.removeParameter("obj");
+        result = fixture.numParameters();
+        assertEquals(0, result);
+    }
+
+    /**
+     * Run the void removePattern(int) method test.
+     * 
+     * @throws Exception
+     * 
+     */
+    @Test
+    public void testRemovePattern_1() throws Exception {
+        Effect fixture = GsonUtil.deserialize(json, Effect.class);
+        assertEquals(1, fixture.numPatterns());
+        int index = 0;
+        fixture.removePattern(index);
+        assertEquals(0, fixture.numPatterns());
+    }
+
+    /**
+     * Run the void setCode(String) method test.
+     * 
+     * @throws Exception
+     * 
+     */
+    @Test
+    public void testSetCode_1() throws Exception {
+        Effect fixture = GsonUtil.deserialize(json, Effect.class);
+        String code = "new code";
+
+        fixture.setCode(code);
+
+        assertEquals("new code", fixture.getCode());
+    }
+
+    /**
+     * Run the void setPattern(int,String) method test.
+     * 
+     * @throws Exception
+     * 
+     */
+    @Test
+    public void testSetPattern_1() throws Exception {
+        Effect fixture = GsonUtil.deserialize(json, Effect.class);
+        fixture.setPattern(0, "new pattern");
+
+        assertEquals("new pattern", fixture.getPattern(0));
+    }
+    
+    /**
      * Perform pre-test initialization.
      * 
      * @throws Exception
