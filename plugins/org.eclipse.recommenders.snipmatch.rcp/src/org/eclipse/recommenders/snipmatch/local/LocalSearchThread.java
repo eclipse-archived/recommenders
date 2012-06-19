bd50494df331d2e6fe00fa492d95ce48e6bc23bb
Cheng Chen <chengchendoc@gmail.com>
2012-06-19 15:24:09 +0800

    [snipmatch] fix license text for SnipMatch java classes source

--- plugins/org.eclipse.recommenders.snipmatch.rcp/src/org/eclipse/recommenders/snipmatch/local/LocalSearchThread.java
+++ plugins/org.eclipse.recommenders.snipmatch.rcp/src/org/eclipse/recommenders/snipmatch/local/LocalSearchThread.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2012 Doug Wightman, Zi Ye, Cheng Chen
+ * Copyright (c) 2011,2012 Doug Wightman, Zi Ye, Cheng Chen
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
--- plugins/org.eclipse.recommenders.snipmatch.rcp/src/org/eclipse/recommenders/snipmatch/preferences/PreferenceInitializer.java
+++ plugins/org.eclipse.recommenders.snipmatch.rcp/src/org/eclipse/recommenders/snipmatch/preferences/PreferenceInitializer.java
@@ -1,13 +1,13 @@
 /**
- * Copyright (c) 2011 Doug Wightman, Zi Ye, Cheng Chen
+ * Copyright (c) 2012 Cheng Chen
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  *
- * Contributors:
- *    Cheng Chen - initial API and implementation.
- */
+ *  Contributors:
+ *    Cheng Chen - initial API and implementation and/or initial documentation
+*/
 package org.eclipse.recommenders.snipmatch.preferences;
 
 import java.io.File;
