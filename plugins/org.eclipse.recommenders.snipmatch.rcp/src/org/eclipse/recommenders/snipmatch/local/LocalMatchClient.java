89d7f82f8db1259d5c6d538311e2647bd18dc35c
Cheng Chen <chengchendoc@gmail.com>
2012-06-19 15:51:24 +0800

    [snipmatch] improve license text for 4 class file
    Change-Id: I3d841e08a72923cb5ad8e5589551e8039ff5a4c9
    Signed-off-by: Cheng Chen <chengchendoc@gmail.com>

--- plugins/org.eclipse.recommenders.snipmatch.rcp/src/org/eclipse/recommenders/snipmatch/local/LocalMatchClient.java
+++ plugins/org.eclipse.recommenders.snipmatch.rcp/src/org/eclipse/recommenders/snipmatch/local/LocalMatchClient.java
@@ -4,6 +4,10 @@
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *    Doug Wightman Zi Ye - initial API and implementation.
+ *    Cheng Chen - change for local search mode SnipMatch
  */
 
 package org.eclipse.recommenders.snipmatch.local;
