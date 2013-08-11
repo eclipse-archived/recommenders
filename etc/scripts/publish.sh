928ef7c5deac3bb2e3b3db9120a0ba475f4f3fae
Marcel Bruch <marcel.bruch@codetrails.com>
2013-08-11 11:51:00 +0200

    [releng] re-added lost formatter settings for >_ and imports.
    * Exported Recommenders code formatter profile
    * changed preference tasks from lastmod to reconcile
    * removed etc/ folder
    
    Change-Id: I9e57a5c0a9e62bb0497eb20ae9ebc91c04052b1e

--- etc/scripts/publish.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-echo "value of channel: $channel"
-echo "value of version: $version"
-rm -rf /home/data/httpd/download.eclipse.org/recommenders/updates/$channel/$version/*
-cp -r  $WORKSPACE/dist/org.eclipse.recommenders.repository.rcp.$channel.$version/target/repository/* /home/data/httpd/download.eclipse.org/recommenders/updates/$channel/$version/
-cp     $WORKSPACE/etc/scripts/index.php /home/data/httpd/download.eclipse.org/recommenders/updates/$channel/$version/
-chmod g+w -R $WORKSPACE/etc/scripts/index.php /home/data/httpd/download.eclipse.org/recommenders/updates/$channel/$version/*
