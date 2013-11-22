ccc046c74d2b138e1041d1916a19cce482c7d227
Marcel Bruch <marcel.bruch@codetrails.com>
2013-11-22 20:08:38 +0100

    Bug 422177: Enable recommenders content assist by default II
    * Ensure configure proposal is not applied automatically because it is
    the only proposal
    * Updated description text to reflect Gunnar's comment
    
    Change-Id: Ic0298a1e4f926e24438c4b3743fd94bc2464a947

--- plugins/org.eclipse.recommenders.completion.rcp/src/org/eclipse/recommenders/internal/completion/rcp/EnableCompletionProposal.java
+++ plugins/org.eclipse.recommenders.completion.rcp/src/org/eclipse/recommenders/internal/completion/rcp/EnableCompletionProposal.java
@@ -13,7 +13,10 @@ package org.eclipse.recommenders.internal.completion.rcp;
 import static org.eclipse.jdt.internal.ui.JavaPlugin.getActiveWorkbenchShell;
 import static org.eclipse.jface.viewers.StyledString.DECORATIONS_STYLER;
 import static org.eclipse.recommenders.internal.completion.rcp.Constants.*;
+import static org.eclipse.recommenders.rcp.utils.PreferencesHelper.createLinkLabelToPreferencePage;
 import static org.eclipse.ui.dialogs.PreferencesUtil.createPreferenceDialogOn;
+
+import java.net.URL;
 
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jdt.internal.ui.text.java.AbstractJavaCompletionProposal;
@@ -25,31 +28,41 @@ import org.eclipse.jface.viewers.StyledString;
 import org.eclipse.recommenders.completion.rcp.DisableContentAssistCategoryJob;
 import org.eclipse.recommenders.rcp.SharedImages;
 import org.eclipse.recommenders.rcp.SharedImages.Images;
-import org.eclipse.recommenders.rcp.utils.PreferencesHelper;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
-import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Link;
 import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.browser.IWebBrowser;
 
 @SuppressWarnings("restriction")
-public class ConfigureCompletionProposal extends AbstractJavaCompletionProposal {
+public class EnableCompletionProposal extends AbstractJavaCompletionProposal {
 
     private static final Object DUMMY_INFO = new Object();
+    private static final String HERE = "here";
+    private static final String HOMEPAGE = "project homepage";
+    private static final String PAGE_NAME = "Preferences > "
+            + createLinkLabelToPreferencePage(COMPLETION_PREFERENCE_PAGE_ID);
+
+    private static final String INFO = "Code Recommenders is available in your Eclipse installation. "
+            + "If you want to enable Code Recommenders to get a smarter content assist, simply press return.\n\n"
+            + "To configure Code Recommenders first, visit <a>" + PAGE_NAME + "</a>.\n\n"
+            + "To disable Code Recommenders click <a>" + HERE
+            + "</a>. To learn more about Code Recommenders please visit the <a>" + HOMEPAGE + "</a>.";
 
     // leave a bit space for other, maybe more important proposals
     private static final int RELEVANCE = Integer.MAX_VALUE - 10000;
 
-    public ConfigureCompletionProposal(SharedImages images) {
+    public EnableCompletionProposal(SharedImages images, int offset) {
         Image image = images.getImage(Images.OBJ_LIGHTBULB);
         StyledString text = new StyledString("Enable intelligent code completion?", DECORATIONS_STYLER);
         setStyledDisplayString(text);
         setImage(image);
         setRelevance(RELEVANCE);
+        setCursorPosition(offset);
     }
 
     @Override
@@ -58,12 +71,17 @@ public class ConfigureCompletionProposal extends AbstractJavaCompletionProposal 
     }
 
     @Override
+    protected boolean isPrefix(String prefix, String string) {
+        return true;
+    }
+
+    @Override
     public IInformationControlCreator getInformationControlCreator() {
         return new IInformationControlCreator() {
 
             @Override
             public IInformationControl createInformationControl(Shell parent) {
-                return new ConfigureContentAssistInformationControl(parent, "Status");
+                return new ConfigureContentAssistInformationControl(parent, "Eclipse Code Recommenders");
             }
         };
     }
@@ -91,27 +109,41 @@ public class ConfigureCompletionProposal extends AbstractJavaCompletionProposal 
 
         @Override
         protected void createContent(Composite parent) {
-            Display display = parent.getDisplay();
-            Color bg = display.getSystemColor(SWT.COLOR_INFO_BACKGROUND);
-            final String prefPageLabel = PreferencesHelper
-                    .createLinkLabelToPreferencePage(COMPLETION_PREFERENCE_PAGE_ID);
-
             Link link = new Link(parent, SWT.NONE);
-            link.setBackground(bg);
-            link.setText("I've recognized you have enabled Code Recommenders but didn't configure it properly yet.\n\n"
-                    + "To enable Code Recommenders with its defaults, simply press return. "
-                    + "If you want to configure it now, go to <a>" + prefPageLabel + "</a> preference page.\n\n"
-                    + "Click <a>here</a> to disable Code Recommenders in your content assist.");
+            link.setBackground(parent.getBackground());
+            link.setText(INFO);
+
             link.addSelectionListener(new SelectionAdapter() {
                 @Override
                 public void widgetSelected(SelectionEvent e) {
                     dispose();
-                    String text = e.text;
-                    if ("here".equalsIgnoreCase(text)) {
-                        new DisableContentAssistCategoryJob(RECOMMENDERS_ALL_CATEGORY_ID).schedule();
-                    } else if (prefPageLabel.equalsIgnoreCase(text)) {
-                        createPreferenceDialogOn(getActiveWorkbenchShell(), COMPLETION_PREFERENCE_PAGE_ID, null, null)
-                                .open();
+                    if (HERE.equals(e.text)) {
+                        diableCodeRecommenders();
+                    } else if (PAGE_NAME.equals(e.text)) {
+                        openPreferencePage();
+                    } else if (HOMEPAGE.equals(e.text)) {
+                        openHomepageInBrowser();
+                    }
+                }
+
+                private void diableCodeRecommenders() {
+                    new DisableContentAssistCategoryJob(RECOMMENDERS_ALL_CATEGORY_ID).schedule();
+                }
+
+                private void openPreferencePage() {
+                    createPreferenceDialogOn(getActiveWorkbenchShell(), COMPLETION_PREFERENCE_PAGE_ID, null, null)
+                            .open();
+                }
+
+                private void openHomepageInBrowser() {
+                    try {
+                        IWebBrowser browser = PlatformUI
+                                .getWorkbench()
+                                .getBrowserSupport()
+                                .createBrowser(SWT.NONE, "recommenders-homepage", "Code Recommenders",
+                                        "Learn about Code Recommenders");
+                        browser.openURL(new URL("http://eclipse.org/recommenders"));
+                    } catch (Exception e1) {
                     }
                 }
             });
