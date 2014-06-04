17d2a49113b178fa9cf1d4c79ff456ec74cc0ef6
Johannes Dorn <johannes.dorn@codetrails.com>
2014-06-04 14:58:43 +0200

    [Snipmatch] Bug 436574: Added missing template variable resolvers
    Added resolvers for the following variables:
     - argType
     - localVar
     - field
     - exception_variable_name
     - importStatic
    
    Change-Id: Ia628a911a9d687db16292570d9b1c5db8cc6e763

--- plugins/org.eclipse.recommenders.snipmatch.rcp/src/org/eclipse/recommenders/internal/snipmatch/rcp/SnipmatchContentAssistProcessor.java
+++ plugins/org.eclipse.recommenders.snipmatch.rcp/src/org/eclipse/recommenders/internal/snipmatch/rcp/SnipmatchContentAssistProcessor.java
@@ -21,12 +21,17 @@ import javax.inject.Inject;
 import org.apache.commons.lang3.StringUtils;
 import org.eclipse.jdt.core.ICompilationUnit;
 import org.eclipse.jdt.internal.corext.template.java.ElementTypeResolver;
+import org.eclipse.jdt.internal.corext.template.java.ExceptionVariableNameResolver;
+import org.eclipse.jdt.internal.corext.template.java.FieldResolver;
 import org.eclipse.jdt.internal.corext.template.java.ImportsResolver;
 import org.eclipse.jdt.internal.corext.template.java.JavaContext;
 import org.eclipse.jdt.internal.corext.template.java.JavaContextType;
 import org.eclipse.jdt.internal.corext.template.java.LinkResolver;
+import org.eclipse.jdt.internal.corext.template.java.LocalVarResolver;
 import org.eclipse.jdt.internal.corext.template.java.NameResolver;
+import org.eclipse.jdt.internal.corext.template.java.StaticImportResolver;
 import org.eclipse.jdt.internal.corext.template.java.TypeResolver;
+import org.eclipse.jdt.internal.corext.template.java.TypeVariableResolver;
 import org.eclipse.jdt.internal.corext.template.java.VarResolver;
 import org.eclipse.jdt.internal.ui.javaeditor.EditorUtility;
 import org.eclipse.jdt.ui.text.java.JavaContentAssistInvocationContext;
@@ -84,9 +89,21 @@ public class SnipmatchContentAssistProcessor implements IContentAssistProcessor 
         importResolver.setType("import"); //$NON-NLS-1$
         contextType.addResolver(importResolver);
 
+        StaticImportResolver staticImportResolver = new StaticImportResolver();
+        staticImportResolver.setType("importStatic"); //$NON-NLS-1$
+        contextType.addResolver(staticImportResolver);
+
         VarResolver varResolver = new VarResolver();
         varResolver.setType("var"); //$NON-NLS-1$
         contextType.addResolver(varResolver);
+
+        LocalVarResolver localVarResolver = new LocalVarResolver();
+        localVarResolver.setType("localVar"); //$NON-NLS-1$
+        contextType.addResolver(localVarResolver);
+
+        FieldResolver fieldResolver = new FieldResolver();
+        fieldResolver.setType("field"); //$NON-NLS-1$
+        contextType.addResolver(fieldResolver);
 
         TypeResolver typeResolver = new TypeResolver();
         typeResolver.setType("newType"); //$NON-NLS-1$
@@ -103,6 +120,14 @@ public class SnipmatchContentAssistProcessor implements IContentAssistProcessor 
         ElementTypeResolver elementTypeResolver = new ElementTypeResolver();
         elementTypeResolver.setType("elemType"); //$NON-NLS-1$
         contextType.addResolver(elementTypeResolver);
+
+        TypeVariableResolver typeVariableResolver = new TypeVariableResolver();
+        typeVariableResolver.setType("argType"); //$NON-NLS-1$
+        contextType.addResolver(typeVariableResolver);
+
+        ExceptionVariableNameResolver exceptionVariableNameResolver = new ExceptionVariableNameResolver();
+        exceptionVariableNameResolver.setType("exception_variable_name"); //$NON-NLS-1$
+        contextType.addResolver(exceptionVariableNameResolver);
 
         return contextType;
     }
