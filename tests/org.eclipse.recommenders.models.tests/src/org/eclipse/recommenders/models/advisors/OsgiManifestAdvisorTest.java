e89ddac74950b5fe18d6604b4d0b705718a55892
Andreas Sewe <andreas.sewe@codetrails.com>
2014-08-14 10:18:48 +0200

    [models] Bug 441751: IAE thrown by advisor on non-domain bundle names
    Change-Id: Ic22f771c7bf13612d3be74445795605140cdb0e8
    Signed-off-by: Andreas Sewe <andreas.sewe@codetrails.com>

--- tests/org.eclipse.recommenders.models.tests/src/org/eclipse/recommenders/models/advisors/OsgiManifestAdvisorTest.java
+++ tests/org.eclipse.recommenders.models.tests/src/org/eclipse/recommenders/models/advisors/OsgiManifestAdvisorTest.java
@@ -11,43 +11,112 @@
 package org.eclipse.recommenders.models.advisors;
 
 import static com.google.common.base.Optional.*;
-import static org.eclipse.recommenders.models.DependencyType.JAR;
-import static org.junit.Assert.*;
+import static org.eclipse.recommenders.models.DependencyType.*;
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.assertThat;
 import static org.mockito.Mockito.*;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Collection;
+import java.util.LinkedList;
 import java.util.jar.Attributes;
 import java.util.jar.JarFile;
 import java.util.jar.Manifest;
 
 import org.eclipse.recommenders.models.DependencyInfo;
-import org.eclipse.recommenders.models.DependencyType;
 import org.eclipse.recommenders.models.IProjectCoordinateAdvisor;
 import org.eclipse.recommenders.models.ProjectCoordinate;
+import org.eclipse.recommenders.utils.Nullable;
 import org.eclipse.recommenders.utils.Zips.IFileToJarFileConverter;
-import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
 
 import com.google.common.base.Optional;
+import com.google.common.collect.Lists;
 
+@RunWith(Parameterized.class)
 public class OsgiManifestAdvisorTest {
 
     @Rule
     public final TemporaryFolder folder = new TemporaryFolder();
 
-    private File jarFileExample;
+    private final String bundleSymbolicName;
+    private final String bundleVersion;
+    private final Optional<ProjectCoordinate> expectedCoordinate;
 
-    private DependencyInfo info;
+    public OsgiManifestAdvisorTest(String description, String bundleSymbolicName, String bundleVersion,
+            @Nullable ProjectCoordinate expectedCoordinate) {
+        this.bundleSymbolicName = bundleSymbolicName;
+        this.bundleVersion = bundleVersion;
+        this.expectedCoordinate = Optional.fromNullable(expectedCoordinate);
+    }
 
-    @Before
-    public void init() throws IOException {
-        jarFileExample = folder.newFile("example.jar");
-        info = new DependencyInfo(jarFileExample, JAR);
+    @Parameters(name = "{index}: {0} ({1};bundle-version=\"{2}\")")
+    public static Collection<Object[]> scenarios() {
+        LinkedList<Object[]> scenarios = Lists.newLinkedList();
+
+        scenarios.add(scenario("One-part Bundle-SymbolicName becomes groupId", "example", "1.0.0",
+                new ProjectCoordinate("example", "example", "1.0.0")));
+        scenarios.add(scenario("Two-part Bundle-SymbolicName becomes groupId", "org.example", "1.0.0",
+                new ProjectCoordinate("org.example", "org.example", "1.0.0")));
+        scenarios.add(scenario("Three-part Bundle-SymbolicName becomes groupId", "org.example.project", "1.0.0",
+                new ProjectCoordinate("org.example.project", "org.example.project", "1.0.0")));
+
+        scenarios.add(scenario("The groupId is limited to first part of unknown top-level domain", "javax.beans",
+                "1.0.0", new ProjectCoordinate("javax", "javax.beans", "1.0.0")));
+        scenarios.add(scenario("The groupId is limited to first three parts of Bundle-SymbolicName",
+                "org.example.project.test", "1.0.0", new ProjectCoordinate("org.example.project",
+                        "org.example.project.test", "1.0.0")));
+
+        scenarios.add(scenario("Qualifier of Bundle-Version is stripped", "org.example.project", "1.0.0.qualifier",
+                new ProjectCoordinate("org.example.project", "org.example.project", "1.0.0")));
+        scenarios.add(scenario("Expanded qualifier of Bundle-Version is stripped", "org.example.project",
+                "1.0.0.v20140814-1000", new ProjectCoordinate("org.example.project", "org.example.project", "1.0.0")));
+
+        scenarios.add(scenario("Directories of Bundle-SymbolicName are stripped", "org.example.project;singleton=true",
+                "1.0.0", new ProjectCoordinate("org.example.project", "org.example.project", "1.0.0")));
+
+        scenarios.add(scenario("Invalid Bundle-Version", "org.example.project", "1.0-Beta", null));
+        // See https://bugs.eclipse.org/bugs/show_bug.cgi?id=441751
+        scenarios
+        .add(scenario("Invalid domain name in Bundle-SymbolicName", "org.example.project._test", "1.0.0", null));
+
+        return scenarios;
+    }
+
+    private static Object[] scenario(String description, String bundleSymbolicName, String bundleVersion,
+            ProjectCoordinate expectedCoordinate) {
+        return new Object[] { description, bundleSymbolicName, bundleVersion, expectedCoordinate };
+    }
+
+    @Test
+    public void testManifestInJarDependency() throws Exception {
+        File jarFile = folder.newFile("dependency.jar");
+        DependencyInfo info = new DependencyInfo(jarFile, JAR);
+        IFileToJarFileConverter fileToJarFileConverter = createFileToJarFileConverter(bundleSymbolicName, bundleVersion);
+
+        IProjectCoordinateAdvisor sut = new OsgiManifestAdvisor(fileToJarFileConverter);
+        Optional<ProjectCoordinate> suggestion = sut.suggest(info);
+
+        assertThat(suggestion, is(equalTo(expectedCoordinate)));
+    }
+
+    @Test
+    public void testManifestInProjectDependency() throws Exception {
+        File projectDirectory = createProjectWithManifestFile("TestProject", bundleSymbolicName, bundleVersion);
+        DependencyInfo info = new DependencyInfo(projectDirectory, PROJECT);
+
+        IProjectCoordinateAdvisor sut = new OsgiManifestAdvisor();
+        Optional<ProjectCoordinate> suggestion = sut.suggest(info);
+
+        assertThat(suggestion, is(equalTo(expectedCoordinate)));
     }
 
     private IFileToJarFileConverter createFileToJarFileConverter(String bundleName, String bundleVersion) {
@@ -70,124 +139,26 @@ public class OsgiManifestAdvisorTest {
         return fileToJarFileConverter;
     }
 
+    private File createProjectWithManifestFile(String projectName, String bundleSymbolicName, String bundleVersion)
+            throws IOException, FileNotFoundException {
+        File projectFolder = folder.newFolder(projectName);
+        File metaInfFolder = folder.newFolder(projectName, "META-INF");
+        File manifestFile = new File(metaInfFolder, "MANIFEST.MF");
+        manifestFile.createNewFile();
+
+        Manifest manifest = createManifest(bundleSymbolicName, bundleVersion);
+        FileOutputStream fileOutputStream = new FileOutputStream(manifestFile);
+        manifest.write(fileOutputStream);
+        fileOutputStream.close();
+
+        return projectFolder;
+    }
+
     private Manifest createManifest(String bundleName, String bundleVersion) {
         final Manifest manifest = new Manifest();
         manifest.getMainAttributes().putValue(OsgiManifestAdvisor.BUNDLE_NAME.toString(), bundleName);
         manifest.getMainAttributes().putValue(OsgiManifestAdvisor.BUNDLE_VERSION.toString(), bundleVersion);
         manifest.getMainAttributes().putValue(Attributes.Name.MANIFEST_VERSION.toString(), "1.0");
         return manifest;
-    }
-
-    @Test
-    public void testOnePartSymbolicNameBecomesGroupId() {
-        IFileToJarFileConverter fileToJarFileConverter = createFileToJarFileConverter("example", "1.0.0");
-        IProjectCoordinateAdvisor sut = new OsgiManifestAdvisor(fileToJarFileConverter);
-        Optional<ProjectCoordinate> optionalProjectCoordinate = sut.suggest(info);
-
-        ProjectCoordinate expected = new ProjectCoordinate("example", "example", "1.0.0");
-        assertEquals(expected, optionalProjectCoordinate.get());
-    }
-
-    @Test
-    public void testTwoPartSymbolicNameBecomesGroupId() {
-        IFileToJarFileConverter fileToJarFileConverter = createFileToJarFileConverter("org.example", "1.0.0");
-        IProjectCoordinateAdvisor sut = new OsgiManifestAdvisor(fileToJarFileConverter);
-        Optional<ProjectCoordinate> optionalProjectCoordinate = sut.suggest(info);
-
-        ProjectCoordinate expected = new ProjectCoordinate("org.example", "org.example", "1.0.0");
-        assertEquals(expected, optionalProjectCoordinate.get());
-    }
-
-    @Test
-    public void testFirstPartOfUnknownTopLevelDomainBecomesGroupId() {
-        IFileToJarFileConverter fileToJarFileConverter = createFileToJarFileConverter("x.example", "1.0.0");
-        IProjectCoordinateAdvisor sut = new OsgiManifestAdvisor(fileToJarFileConverter);
-        Optional<ProjectCoordinate> optionalProjectCoordinate = sut.suggest(info);
-
-        ProjectCoordinate expected = new ProjectCoordinate("x", "x.example", "1.0.0");
-        assertEquals(expected, optionalProjectCoordinate.get());
-    }
-
-    @Test
-    public void testThreePartSymbolicNameBecomesGroupId() {
-        IFileToJarFileConverter fileToJarFileConverter = createFileToJarFileConverter("org.example.sample", "1.0.0");
-        IProjectCoordinateAdvisor sut = new OsgiManifestAdvisor(fileToJarFileConverter);
-        Optional<ProjectCoordinate> optionalProjectCoordinate = sut.suggest(info);
-
-        ProjectCoordinate expected = new ProjectCoordinate("org.example.sample", "org.example.sample", "1.0.0");
-        assertEquals(expected, optionalProjectCoordinate.get());
-    }
-
-    @Test
-    public void testFirstThreePartsOfSymbolicNameBecomesGroupId() {
-        IFileToJarFileConverter fileToJarFileConverter = createFileToJarFileConverter("org.example.sample.test",
-                "1.0.0");
-        IProjectCoordinateAdvisor sut = new OsgiManifestAdvisor(fileToJarFileConverter);
-        Optional<ProjectCoordinate> optionalProjectCoordinate = sut.suggest(info);
-
-        ProjectCoordinate expected = new ProjectCoordinate("org.example.sample", "org.example.sample.test", "1.0.0");
-        assertEquals(expected, optionalProjectCoordinate.get());
-    }
-
-    @Test
-    public void extractProjectCoordinateForProjectCorrect() throws IOException {
-        File projectFolder = createProjectWithManifestFile("TestProject", "org.example.sample.test", "1.0.0");
-
-        DependencyInfo info = new DependencyInfo(projectFolder, DependencyType.PROJECT);
-        ProjectCoordinate expected = new ProjectCoordinate("org.example.sample", "org.example.sample.test", "1.0.0");
-
-        IProjectCoordinateAdvisor sut = new OsgiManifestAdvisor();
-        Optional<ProjectCoordinate> optionalProjectCoordinate = sut.suggest(info);
-
-        assertEquals(expected, optionalProjectCoordinate.get());
-    }
-
-    @Test
-    public void extractProjectCoordinateForProjectCorrectWhenBundleVersionContainsQualifier() throws IOException {
-        File projectFolder = createProjectWithManifestFile("TestProject", "org.example.sample.test", "1.0.0.qualifier");
-
-        DependencyInfo info = new DependencyInfo(projectFolder, DependencyType.PROJECT);
-        ProjectCoordinate expected = new ProjectCoordinate("org.example.sample", "org.example.sample.test", "1.0.0");
-
-        IProjectCoordinateAdvisor sut = new OsgiManifestAdvisor();
-        Optional<ProjectCoordinate> optionalProjectCoordinate = sut.suggest(info);
-
-        assertEquals(expected, optionalProjectCoordinate.get());
-    }
-
-    @Test
-    public void extractProjectCoordinateForProjectCorrectWhenBundleNameContainsDirective() throws IOException {
-        File projectFolder = createProjectWithManifestFile("TestProject", "org.example.sample.test;singleton:=true",
-                "1.0.0.qualifier");
-
-        DependencyInfo info = new DependencyInfo(projectFolder, DependencyType.PROJECT);
-        ProjectCoordinate expected = new ProjectCoordinate("org.example.sample", "org.example.sample.test", "1.0.0");
-
-        IProjectCoordinateAdvisor sut = new OsgiManifestAdvisor();
-        Optional<ProjectCoordinate> optionalProjectCoordinate = sut.suggest(info);
-
-        assertEquals(expected, optionalProjectCoordinate.get());
-    }
-
-    @Test
-    public void testInvalidVersionResultInAbsent() {
-        IFileToJarFileConverter fileToJarFileConverter = createFileToJarFileConverter("org.example.sample", "1.0-Beta");
-        IProjectCoordinateAdvisor sut = new OsgiManifestAdvisor(fileToJarFileConverter);
-        Optional<ProjectCoordinate> optionalProjectCoordinate = sut.suggest(info);
-
-        assertFalse(optionalProjectCoordinate.isPresent());
-    }
-
-    private File createProjectWithManifestFile(String projectName, String bundleName, String bundleVersion)
-            throws IOException, FileNotFoundException {
-        File projectFolder = folder.newFolder(projectName);
-        File manifestFile = new File(projectFolder, "META-INF" + File.separator + "MANIFEST.MF");
-        manifestFile.getParentFile().mkdir();
-        manifestFile.createNewFile();
-        Manifest manifest = createManifest(bundleName, bundleVersion);
-        FileOutputStream fileOutputStream = new FileOutputStream(manifestFile);
-        manifest.write(fileOutputStream);
-        fileOutputStream.close();
-        return projectFolder;
     }
 }
