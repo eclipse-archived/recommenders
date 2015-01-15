698fb69246c36f31b55c926b24ac34320f1fab57
simonlaffoy <simon.laffoy@codetrails.com>
2015-01-15 17:26:51 +0100

    [stacktraces] Bug 457347: Grammar error in "An Error Was Logged"
    Change-Id: Ia82b1157832c04178a2cfceaa42997f964f2125c
    Signed-off-by: simonlaffoy <simon.laffoy@codetrails.com>

--- plugins/org.eclipse.recommenders.stacktraces.rcp/src/org/eclipse/recommenders/internal/stacktraces/rcp/ErrorReportDialog.java
+++ plugins/org.eclipse.recommenders.stacktraces.rcp/src/org/eclipse/recommenders/internal/stacktraces/rcp/ErrorReportDialog.java
@@ -87,7 +87,7 @@ public class ErrorReportDialog extends MessageDialog {
                 parentShell,
                 "An Error Was Logged",
                 TITLE_IMAGE_DESC.createImage(),
-                "We noticed a new error event was logged. Such error events may reveal issues in the Eclipse codebase, and thus we kindly ask you to report it to eclipse.org.",
+                "We noticed a new error event was logged. Such error events may reveal issues in the Eclipse codebase, and thus we kindly ask you to report them to eclipse.org.",
                 MessageDialog.WARNING, new String[] { "Send", "Don't Send" }, 0);
         setShellStyle(SWT.MODELESS | SWT.DIALOG_TRIM | SWT.RESIZE | SWT.MAX);
         setBlockOnOpen(false);
