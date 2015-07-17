413b2c4f1eeccebff1c742340cd26597d33e641c
Andreas Sewe <andreas.sewe@codetrails.com>
2015-07-17 14:07:21 +0200

    [releng] Fix typo in publish-repository.sh script
    Change-Id: Ied3254348e7dfb7f93b477d6ddef8d3015f6e2ec
    Signed-off-by: Andreas Sewe <andreas.sewe@codetrails.com>

--- tools/hudson/publish-repository.sh
+++ tools/hudson/publish-repository.sh
@@ -15,13 +15,13 @@ else
 fi
 
 if [ -z "$3" ]; then
-   echo "No download path (e.g., 'downloads.eclipse.org/recommenders/updates/head') supplied."
+   echo "No download path (e.g., 'downloads.eclipse.org/recommenders/updates/stable') supplied."
    exit 3
 else
    DOWNLOAD_PATH=$3
 fi
 
-echo "Publishing '${RELEASE}' repository of '${SIMREL}' configuration at http://${DOWNLOAD_PATH}"
+echo "Publishing '${REPOSITORY}' repository of '${SIMREL}' configuration at http://${DOWNLOAD_PATH}"
 echo
 
 SOURCE=${HUDSON_HOME}/jobs/${PROMOTED_JOB_NAME}/configurations/axis-simrel/${SIMREL}/builds/${PROMOTED_ID}/archive/repositories/${REPOSITORY}/target/repository
@@ -30,3 +30,5 @@ TARGET=/home/data/httpd/${DOWNLOAD_PATH}
 mkdir -p ${TARGET}
 rm -f -R ${TARGET}/*
 cp -v -R ${SOURCE}/* ${TARGET}
+
+echo
