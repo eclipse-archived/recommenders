97e83a569160fc27e52ebac6dd27d48d77cccb48
Johannes Dorn <johannes.dorn@codetrails.com>
2016-01-22 13:46:39 +0100

    [discovery] Read more link in catalog doesn't react to left-click on OS X
    Bug: 486333
    Change-Id: I9d8072c642815fcffaffeeecf54c1154d664a778
    Signed-off-by: Johannes Dorn <johannes.dorn@codetrails.com>

--- plugins/org.eclipse.recommenders.rcp/src/org/eclipse/recommenders/internal/rcp/preferences/ExtensionDiscoveryHandler.java
+++ plugins/org.eclipse.recommenders.rcp/src/org/eclipse/recommenders/internal/rcp/preferences/ExtensionDiscoveryHandler.java
@@ -15,12 +15,26 @@ import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.recommenders.rcp.utils.Dialogs;
 import org.eclipse.recommenders.utils.rcp.preferences.AbstractLinkContributionPage;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.handlers.HandlerUtil;
 
 public class ExtensionDiscoveryHandler extends AbstractHandler {
 
     @Override
-    public Object execute(ExecutionEvent event) throws ExecutionException {
-        Dialogs.newExtensionsDiscoveryDialog(event.getParameter(AbstractLinkContributionPage.COMMAND_HREF_ID)).open();
+    public Object execute(final ExecutionEvent event) throws ExecutionException {
+        Shell shell = HandlerUtil.getActiveShell(event);
+        Display display = shell != null ? shell.getDisplay() : PlatformUI.getWorkbench().getDisplay();
+
+        display.asyncExec(new Runnable() {
+
+            @Override
+            public void run() {
+                Dialogs.newExtensionsDiscoveryDialog(event.getParameter(AbstractLinkContributionPage.COMMAND_HREF_ID))
+                        .open();
+            }
+        });
         return null;
     }
 }
